e81ffa0525cc50af3529a67295d8e59a18ef3b2b
2682487033@qq.com <hly001925>
2020-10-21 09:26:47 +0800

    修改

--- src/main/java/com/antelopeh/home/controller/MsgController.java
+++ src/main/java/com/antelopeh/home/controller/MsgController.java
@@ -1,15 +1,21 @@
 package com.antelopeh.home.controller;
 
+import com.antelopeh.core.util.WebUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/msg")
 public class MsgController{
 
     @RequestMapping("/teacher")
-    public ModelAndView teacher(){
+    public ModelAndView teacher(HttpServletRequest request, HttpServletResponse response, Model model){
+        model.addAttribute("menuMap", WebUtils.getMenuMap(request));
         return new ModelAndView("msg/teacher/selectMsg");
     }
 }
